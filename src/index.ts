01d1bdbb3706f2ccd77d75cea34fa5f126eb10bb
Bryan Chu <bryanchu10@gmail.com>
2025-11-06 16:49:35 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { rules as relicRules } from "eslint-plugin-relics"
+
+export const configs = {
+    recommended: {
+        plugins: {
+            relics: {
+                relicRules,
+            },
+        },
+        rules: {
+            "relics/enforce-foo-bar": "error",
+            "relics/prefer-explicit-property-checks": "error",
+            "relics/prefer-loose-equality": "error",
+        },
+    },
+    "recommended-legacy": {
+        plugins: ["relics"],
+        rules: {
+            "relics/enforce-foo-bar": "error",
+            "relics/prefer-explicit-property-checks": "error",
+            "relics/prefer-loose-equality": "error",
+        },
+    },
+}
+
+export default {
+    configs,
+};
