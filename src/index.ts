54523f4954531a999fc1dcc120f9c741c641902f
Bryan Chu <bryanchu10@gmail.com>
2025-11-10 16:08:53 +0800

    修正 config 結構

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import { rules as relicRules } from "eslint-plugin-relics"
 
+const recommendedLegacy = {
+    plugins: ["relics"],
+    rules: {
+        "relics/enforce-foo-bar": "error",
+        "relics/prefer-explicit-property-checks": "error",
+        "relics/prefer-loose-equality": "error",
+    },
+};
+
 export const configs = {
     recommended: {
         plugins: {
@@ -13,16 +22,10 @@ export const configs = {
             "relics/prefer-loose-equality": "error",
         },
     },
-    "recommended-legacy": {
-        plugins: ["relics"],
-        rules: {
-            "relics/enforce-foo-bar": "error",
-            "relics/prefer-explicit-property-checks": "error",
-            "relics/prefer-loose-equality": "error",
-        },
-    },
-}
+    "recommended-legacy": recommendedLegacy,
+};
 
 export default {
     configs,
+    "recommended-legacy": recommendedLegacy,
 };
